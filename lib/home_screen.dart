c48d5a09070074eb8f68682f346c4dc70177b480
rizki <mohamadrizki8@gmail.com>
2025-04-22 21:20:42 +0700

    feat: Add responsive layout for start and home screens
    This commit enhances the application's responsiveness by implementing adaptive layouts for both the start and home screens. The start screen now displays a different layout based on the screen width, switching between a mobile-friendly version and a web-optimized version with an image alongside the content. The home screen utilizes a `GridView` with a dynamic `crossAxisCount` that adjusts the number of columns based on the available screen width, ensuring a more comfortable viewing experience across various devices.

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -18,66 +18,94 @@ class HomeScreen extends StatelessWidget {
           IconButton(icon: Icon(Icons.account_circle), onPressed: () {}),
         ],
       ),
-      body: GridView.count(
-        crossAxisCount: 2,
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth <= 750) {
+            return DessertGrid(gridCount: 2);
+          } else if (constraints.maxWidth <= 1200) {
+            return DessertGrid(gridCount: 4);
+          } else {
+            return DessertGrid(gridCount: 6);
+          }
+        }
+      ),
+    );
+  }
+}
+
+class DessertGrid extends StatelessWidget {
+  final int gridCount;
+
+  const DessertGrid({super.key, required this.gridCount});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: GridView.count(
+        crossAxisCount: gridCount,
         children:
-            dessertList.map((dessert) {
-              return InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => DetailScreen(dessert: dessert,)),
-                  );
-                },
-                child: Card(
-                  elevation: 0,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        Image.asset(dessert.imageAsset, height: 120,),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(dessert.name,
+        dessertList.map((dessert) {
+          return InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => DetailScreen(dessert: dessert,)),
+              );
+            },
+            child: Card(
+              elevation: 0,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Expanded(flex: 2, child: Image.asset(dessert.imageAsset)),
+                    SizedBox(height: 8.0,),
+                    Expanded(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(dessert.name,
                                 textAlign: TextAlign.start,
                                 style: TextStyle(
                                   fontSize: 16,
                                   fontWeight: FontWeight.w500,
                                 ),),
-                                Row(
-                                  children: [
-                                    Text("Rp ",
+                              Row(
+                                children: [
+                                  Text("Rp ",
                                     style: TextStyle(
-                                      color: Colors.grey
+                                        color: Colors.grey
                                     ),),
-                                    Text(dessert.price,
-                                      style: TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w500,
-                                      ),
+                                  Text(dessert.price,
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500,
                                     ),
-                                  ],
-                                )
+                                  ),
+                                ],
+                              )
 
-                              ],
-                            ),
-                            IconButton(
-                              onPressed: () {},
-                              icon: Icon(Icons.add_circle),
-                            ),
-                          ],
-                        ),
-                      ],
+                            ],
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.add_circle),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-              );
-            }).toList(),
+              ),
+            ),
+          );
+        }).toList(),
       ),
     );
   }
+
 }
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -7,6 +7,23 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth > 800) {
+            return StartWebPage();
+          } else {
+            return StartMobilePage();
+          }
+        },
+      );
+  }
+}
+
+class StartMobilePage extends StatelessWidget {
+  const StartMobilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFFCD57C),
       body: Stack(
@@ -24,7 +41,7 @@ class StartScreen extends StatelessWidget {
                 Card(
                   elevation: 0,
                   shape : RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(24.0)
+                      borderRadius: BorderRadius.circular(24.0)
                   ),
                   margin: const EdgeInsets.all(24.0),
                   child: Padding(
@@ -75,4 +92,82 @@ class StartScreen extends StatelessWidget {
       ),
     );
   }
+
+}
+
+class StartWebPage extends StatelessWidget {
+  const StartWebPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFFCD57C),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(child: Image.asset("images/dessert-box.png")),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(120.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Card(
+                      elevation: 0,
+                      shape : RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(24.0)
+                      ),
+                      margin: const EdgeInsets.all(24.0),
+                      child: Padding(
+                        padding: const EdgeInsets.all(24.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              "Temukan Dessert Box Kesukaanmu Disini",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 24.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 8.0),
+                            Text(
+                              "Temukan dan coba dessert box terbaik kami",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                            SizedBox(height: 16.0),
+                            SizedBox(
+                              width: double.infinity,
+                              height: 50.0,
+                              child: FilledButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => const HomeScreen(),
+                                    ),
+                                  );
+                                },
+                                child: Text(
+                                  'Mulai',
+                                  style: TextStyle(fontSize: 16.0),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
 }
